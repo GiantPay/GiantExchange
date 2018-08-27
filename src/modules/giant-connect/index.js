00d53631cc2105dee327260a8e1f898950b8251a
AbilityG <tursumbekov@mail.ru>
2018-08-27 20:23:30 +0600

    Backend API mock

--- src/modules/giant-connect/index.js
+++ src/modules/giant-connect/index.js
@@ -1,7 +1,7 @@
 import giantConnect from './giant-connect';
 import mockProvider from './mock-provider/index';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   giantConnect.use(mockProvider());
 } else {
   // giantConnect.use(mainnetProvider("http://192.168.0.1"));
---
+++ src/modules/vue-websocket/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import VueWebsocket from 'vue-websocket';
+
+Vue.use(VueWebsocket);
