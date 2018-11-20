cb2c575d98a6bd126cf9ec73260d508dfffc5447
Denis Sel <sfaiross@yandex.ru>
2018-11-20 14:26:34 +0700

    Add tabs and first version table transaction

---
+++ src/components/page-components/Dashboard/TransactionTable.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionTable',
+  props: {
+    dealList: {
+      type: Array,
+    },
+    isLoading: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data: () => ({
+    fields: {
+      date_time: {
+        label: 'Date/Time',
+        sortable: true,
+      },
+      assets: {
+        label: 'Assets',
+        sortable: true,
+      },
+      price: {
+        label: 'Price',
+        sortable: true,
+      },
+      revard: {
+        label: 'Revard',
+        sortable: true,
+      },
+      close_time: {
+        label: 'Closing date/time',
+        sortable: true,
+      },
+      inform: {
+        label: 'Additional information',
+      },
+    },
+    items: [
+      {
+        isActive: true,
+        date_time: '20.11.2018 11:20',
+        assets: '500',
+        price: '0.00058',
+        revard: '15',
+        close_time: '20.11.2018 11:27',
+        inform: 'loren',
+      },
+      {
+        isActive: true,
+        date_time: '20.11.2018 11:21',
+        assets: '300',
+        price: '0.00062',
+        revard: '15',
+        close_time: '20.11.2018 11:23',
+        inform: 'loren',
+      },
+      {
+        isActive: true,
+        date_time: '20.11.2018 11:19',
+        assets: '100',
+        price: '0.00055',
+        revard: '15',
+        close_time: '20.11.2018 11:32',
+        inform: 'loren',
+      },
+      {
+        isActive: true,
+        date_time: '20.11.2018 11:23',
+        assets: '600',
+        price: '0.00059',
+        revard: '15',
+        close_time: '20.11.2018 11:21',
+        inform: 'loren',
+      },
+    ],
+  }),
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,35 @@
 <template>
-<h1>//To do</h1>
+  <b-card no-body>
+    <b-tabs card>
+      <b-tab title="Portfolio" active>
+        <b-row>
+          <b-col cols="12">
+            <h1>List of transaction</h1>
+          </b-col>
+          <b-col cols="12">
+            <TransactionTable></TransactionTable>
+          </b-col>
+        </b-row>
+      </b-tab>
+      <b-tab title="Funds">
+        Tab Contents 2
+      </b-tab>
+      <b-tab title="Activities">
+        Tab Contents 3
+      </b-tab>
+    </b-tabs>
+  </b-card>
 </template>
 
 <script>
+import TransactionTable from '@/components/page-components/Dashboard/TransactionTable.vue';
+
 export default {
   name: 'Dashboard',
+  components: {
+    TransactionTable,
+  },
+  data: () => ({}),
 };
 </script>
 
