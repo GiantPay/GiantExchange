93c0bcac9735f06831a5307da976e75d9b46ddd8
Denis Sel <sfaiross@yandex.ru>
2019-01-31 23:31:42 +0700

    Remove popover select day in date-picker

--- src/modules/v-calendar/index.js
+++ src/modules/v-calendar/index.js
@@ -2,4 +2,6 @@ import Vue from 'vue';
 import VCalendar from 'v-calendar';
 import 'v-calendar/lib/v-calendar.min.css';
 
-Vue.use(VCalendar);
+Vue.use(VCalendar, {
+  datePickerShowDayPopover: false,
+});
