813483dc8e49078be73a6e83b55adfe0f33bc60d
Denis Sel <sfaiross@yandex.ru>
2019-03-25 13:09:35 +0700

    Add general-style.scss, custom-bootstrap-variables.scss, webpack modul for scss files

--- client/stories/index.stories.js
+++ client/stories/index.stories.js
@@ -1,10 +1,14 @@
-/* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
 
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import MyButton from './MyButton';
+import '../src/styles/general-style.scss';
+
 import Welcome from './Welcome';
 
 storiesOf('Welcome', module).add('to Storybook', () => ({
@@ -12,25 +16,3 @@ storiesOf('Welcome', module).add('to Storybook', () => ({
   template: '<welcome :showApp="action" />',
   methods: { action: linkTo('Button') },
 }));
-
-storiesOf('Button', module)
-  .add('with text', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action('clicked') },
-  }))
-  .add('with JSX', () => ({
-    components: { MyButton },
-    // eslint-disable-next-line no-unused-vars
-    render(h) {
-      return <my-button onClick={this.action}>With JSX</my-button>;
-    },
-    methods: { action: linkTo('clicked') },
-  }))
-  .add('with some emoji', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action('clicked') },
-  }));
-
-/* eslint-enable react/react-in-jsx-scope */
