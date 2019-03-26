22b2a02357c04043844aff6844408726397149b6
Denis Sel <sfaiross@yandex.ru>
2019-03-27 00:28:53 +0700

    Check selected options

--- client/src/components/ui-components/Inputs/RadioButtons.vue
+++ client/src/components/ui-components/Inputs/RadioButtons.vue
@@ -15,28 +15,38 @@
 </template>
 
 <script>
-  export default {
-    name: 'RadioButtons',
-    data() {
-      return {
-        selected: 'radio1',
-        options: [
-          { text: 'Radio 1', value: 'radio1' },
-          { text: 'Radio 3', value: 'radio2' },
-          { text: 'Radio 3 (disabled)', value: 'radio3', disabled: true },
-          { text: 'Radio 4', value: 'radio4' },
-        ],
-      };
-    },
-  };
+export default {
+  name: 'RadioButtons',
+  data() {
+    return {
+      selected: 'radio1',
+      options: [
+        { value: 5 * 60 * 1000, text: '5 minutes' },
+        { value: 3 * 60 * 1000, text: '3 minutes' },
+        { value: 60 * 1000, text: '1 minutes' },
+        { value: 30 * 1000, text: '30 sec' },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
   /deep/ .btn-primary {
     margin-right: 5px;
   }
-  .btn-group > .btn:not(:last-child):not(.dropdown-toggle), .btn-group > .btn-group:not(:last-child) > .btn {
+  /deep/ .btn-group > .btn:not(.dropdown-toggle) {
     border-top-right-radius: 5px;
     border-bottom-right-radius: 5px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    background-color: #FFFFFF;
+    color: #4F4F4F;
+    box-shadow: 0 3px 25px rgba(0,0,0,0.1);
+  }
+  /deep/ .active {
+    background-color: #0E5FDA !important;
+    color: #ffffff !important;
+    box-shadow: none;
   }
 </style>
