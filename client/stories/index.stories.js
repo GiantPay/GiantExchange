6c36964304eb0080d388a9c7eb3cb9f93df781ee
Denis Sel <sfaiross@yandex.ru>
2019-04-16 14:30:30 +0700

    Check index.stories.js

--- client/stories/index.stories.js
+++ client/stories/index.stories.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Vuelidate from 'vuelidate'
+import Vuelidate from 'vuelidate';
 import BootstrapVue from 'bootstrap-vue';
 
 Vue.use(Vuelidate);
@@ -20,68 +20,65 @@ storiesOf('Welcome', module).add('to Storybook', () => ({
   methods: { action: linkTo('Button') },
 }));
 
-storiesOf('Inputs', module)
-  .add('InputWithButton', () => ({
-    components: { InputWithButton },
-    template: `<InputWithButton
-                :placeholder="placeholder"
-                :disabled="disabled"
-                v-model="value"        
-                ></InputWithButton>`,
-    data() {
-      return {
-        placeholder: 'Input text',
-        disabled: false,
-      };
-    },
-  }));
+storiesOf('Inputs', module).add('InputWithButton', () => ({
+  components: { InputWithButton },
+  template: `<InputWithButton
+              :placeholder="placeholder"
+              :disabled="disabled"
+              v-model="value"        
+              ></InputWithButton>`,
+  data() {
+    return {
+      placeholder: 'Input text',
+      disabled: false,
+    };
+  },
+}));
 
-storiesOf('Cards', module)
-  .add('CardAssets', () => ({
-    components: { CardAssets },
-    template: `<CardAssets
-                :currencyFirst="currencyFirst"
-                :currencySecond="currencySecond"
-                :oracleValue="oracleValue"
-                :brokerValue="brokerValue"
-                :isActive="isActive"
-                :barValue="barValue"
-                :barVariant="barVariant"                
-                ></CardAssets>`,
-    data() {
-      return {
-        currencyFirst: 'BTC',
-        currencySecond: 'USD',
-        oracleValue: 175,
-        brokerValue: 235,
-        isActive: false,
-        barValue: 75,
-        barVariant: 'success',
-      };
-    },
-  }));
+storiesOf('Cards', module).add('CardAssets', () => ({
+  components: { CardAssets },
+  template: `<CardAssets
+              :currencyFirst="currencyFirst"
+              :currencySecond="currencySecond"
+              :oracleValue="oracleValue"
+              :brokerValue="brokerValue"
+              :isActive="isActive"
+              :barValue="barValue"
+              :barVariant="barVariant"                
+              ></CardAssets>`,
+  data() {
+    return {
+      currencyFirst: 'BTC',
+      currencySecond: 'USD',
+      oracleValue: 175,
+      brokerValue: 235,
+      isActive: false,
+      barValue: 75,
+      barVariant: 'success',
+    };
+  },
+}));
 
-storiesOf('Cards', module)
-  .add('CardAssetsActive', () => ({
-    components: { CardAssets },
-    template: `<CardAssets
-                :currencyFirst="currencyFirst"
-                :currencySecond="currencySecond"
-                :oracleValue="oracleValue"
-                :brokerValue="brokerValue"
-                :isActive="isActive"
-                :barValue="barValue"
-                :barVariant="barVariant"                
-                ></CardAssets>`,
-    data() {
-      return {
-        currencyFirst: 'BTC',
-        currencySecond: 'USD',
-        oracleValue: 175,
-        brokerValue: 235,
-        isActive: true,
-        barValue: 75,
-        barVariant: 'success',
-      };
-    },
-  }));
+storiesOf('Cards', module).add('CardAssetsActive', () => ({
+  components: { CardAssets },
+  template: `<CardAssets
+              :currencyFirst="currencyFirst"
+              :currencySecond="currencySecond"
+              :oracleValue="oracleValue"
+              :brokerValue="brokerValue"
+              :isActive="isActive"
+              :barValue="barValue"
+              :barVariant="barVariant"                
+              ></CardAssets>`,
+  data() {
+    return {
+      currencyFirst: 'BTC',
+      currencySecond: 'USD',
+      oracleValue: 175,
+      brokerValue: 235,
+      isActive: true,
+      barValue: 75,
+      barVariant: 'success',
+    };
+  },
+}));
