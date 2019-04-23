21dc60178125b041161801569f3cb86fdf6ef84e
Ablai <tursumbekov@gmail.com>
2019-04-23 19:13:03 +0600

    Dashboard route tabs

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,61 +1,31 @@
 <template>
   <b-card no-body>
-    <b-tabs card>
-      <b-tab title="Portfolio" active>
-        <b-row>
-          <b-col cols="12">
-            <h1 class="header-transaction">List of transaction</h1>
-          </b-col>
-          <b-col cols="12">
-            <TransactionTable/>
-          </b-col>
-        </b-row>
-      </b-tab>
-      <b-tab title="Funds">
-        <b-col cols="12">
-          <h1 class="header-transaction">Funds</h1>
-        </b-col>
-        <b-col cols="12">
-          <FundsTable/>
-        </b-col>
-      </b-tab>
-      <b-tab title="Activities">
-        <b-col cols="12">
-          <h1 class="header-transaction">Activity</h1>
-        </b-col>
-        <b-col cols="12">
-          <ActivitiesTable/>
-        </b-col>
-      </b-tab>
-    </b-tabs>
+    <b-nav tabs class="nav-head">
+      <b-nav-item active-class="active" :to="{ name: 'transaction-list' }">
+        List of transaction
+      </b-nav-item>
+      <b-nav-item active-class="active" :to="{ name: 'funds' }">
+        Funds
+      </b-nav-item>
+      <b-nav-item active-class="active" :to="{ name: 'activities' }">
+        Activity
+      </b-nav-item>
+    </b-nav>
+    <div class="card-body">
+      <router-view />
+    </div>
   </b-card>
 </template>
 
 <script>
-import TransactionTable from '@/components/page-components/Dashboard/TransactionTable.vue';
-import FundsTable from '@/components/page-components/Dashboard/FundsTable.vue';
-import ActivitiesTable from '@/components/page-components/Dashboard/ActivitiesTable.vue';
-
 export default {
   name: 'Dashboard',
-  components: {
-    TransactionTable,
-    FundsTable,
-    ActivitiesTable,
-  },
-  data: () => ({
-
-  }),
-
-  methods: {
-
-  },
 };
 </script>
 
 <style scoped>
-  .header-transaction {
-    padding-bottom: 30px;
-    padding-left: 12px;
+  .nav-head {
+    padding: 10px 10px 0;
+    background: rgba(0, 0, 0, 0.03);
   }
 </style>
