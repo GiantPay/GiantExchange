d5ceadd37f27991ec28e442c0dd6cff654e7598e
Denis Sel <sfaiross@yandex.ru>
2019-05-15 08:18:04 +0200

    Created component BlockOracles

--- client/src/components/ui-components/Blocks/BlockOracles.vue
+++ client/src/components/ui-components/Blocks/BlockOracles.vue
@@ -2,44 +2,51 @@
   <div class="block-oracles">
     <div class="header">
       <span>Oracles</span>
-      <div class="icon-table">
-
+      <div class="icon-block">
+        <ListUl class="icon"></ListUl>
       </div>
     </div>
     <div class="content">
       <div class="block-title">
         <div class="main-title">
-          <span>Main Title</span>
-          <span>BTC/USD</span>
+          <span class="main-title-text">Main Title</span>
+          <span class="main-title-assets">{{currencyFirst}}/{{ currencySecond }}</span>
         </div>
         <div class="rating">
           <div class="header-rating">
-            <span>Rating</span>
-            <span>7/10</span>
+            <span class="text-rating">Rating</span>
+            <div class="block-rating">
+              <span class="green">{{ ratingValue }}</span>
+              <span class="grey">/10</span>
+            </div>
           </div>
-          <b-progress :value="25" variant="success" striped :animated="animate"></b-progress>
+          <b-progress :value="setBarValue" :variant="setBarVariant"></b-progress>
         </div>
       </div>
       <div class="block-ping">
         <div class="ping">
-
+          <div class="ping-core"></div>
+          <div class="ping-text">
+            <span>Ping: {{pingValue}}</span>
+            <span class="grey tx-8">ms</span>
+          </div>
         </div>
-        <div class="url-oracle">
-          <a href="https://oracleoftheben">https://oracleoftheben</a>
+        <div class="url-block">
+          <a class="blue" :href="urlOracles">{{ urlOracles }}</a>
         </div>
       </div>
       <div class="block-value">
         <div class="brokers">
-          <span>Brokers</span>
-          <span class="value">120</span>
+          <span class="header-value">Brokers</span>
+          <span class="value">{{ brokersValue }}</span>
         </div>
         <div class="options">
-          <span>Options</span>
-          <span class="value">250</span>
+          <span class="header-value">Options</span>
+          <span class="value">{{ optionsValue }}</span>
         </div>
         <div class="fee">
-          <span>Fee</span>
-          <span class="value">0.25%</span>
+          <span class="header-value">Fee</span>
+          <span class="value">{{ feeValue }}</span>
         </div>
       </div>
     </div>
@@ -47,14 +54,235 @@
 </template>
 
 <script>
-  export default {
-    name: "BlockOracles"
+import ListUl from "../../../assets/icons/ListUl.vue";
+
+export default {
+  name: "BlockOracles",
+  components: {
+    ListUl,
+  },
+  props: {
+    currencyFirst: {
+      type: String,
+      default: "BTC",
+    },
+    currencySecond: {
+      type: String,
+      default: "USD",
+    },
+    ratingValue: {
+      type: Number,
+      default: 0,
+    },
+    pingValue: {
+      type: Number,
+      default: 62,
+    },
+    urlOracles: {
+      type: String,
+      default: '',
+    },
+    optionsValue: {
+      type: Number,
+      default: 0,
+    },
+    brokersValue: {
+      type: Number,
+      default: 0,
+    },
+    feeValue: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    setBarValue() {
+      return this.ratingValue * 10;
+    },
+    setBarVariant() {
+      if (this.ratingValue < 3) {
+        return 'danger';
+      } else if (3 >= this.ratingValue && this.ratingValue < 5) {
+        return 'warning';
+      } else {
+        return 'success';
+      }
+    }
   }
+}
 </script>
 
 <style scoped>
 .block-oracles {
   display: flex;
+  flex-direction: column;
+  padding: 20px 30px;
   margin: 5px;
+  max-width: 350px;
+  height: 250px;
+  border-radius: 5px;
+  box-shadow: 0 3px 25px rgba(0, 0, 0, 0.1);
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+  text-transform: uppercase;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+.icon-block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 23px;
+  height: 23px;
+  border-radius: 5px;
+  background-color: #F4F5F7;
+  opacity: 0.6;
+}
+.icon {
+  color: #C7C7C7;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+}
+.block-title {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(199, 199, 199, 0.5);
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+}
+.main-title {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+.main-title-text {
+  font-size: 12px;
+}
+.main-title-assets {
+  font-size: 10px;
+  color: #C7C7C7;
+}
+.rating {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+.header-rating {
+  display: flex;
+  justify-content: space-between;
+  font-size: 12px;
+}
+/deep/.progress {
+  height: 6px;
+}
+.block-ping {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(199, 199, 199, 0.5);
+  padding-top: 10px;
+  padding-bottom: 20px;
+  margin-bottom: 10px;
+}
+.ping {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 50%;
+}
+.ping-core {
+  width: 14px;
+  height: 14px;
+  border-radius: 14px;
+  background-color: #00CC5B;
+  box-shadow: 0 0 0 1px rgba(0, 204, 91, 0.2);
+  animation: pulse 1.5s infinite;
+}
+@-webkit-keyframes pulse {
+  0% {
+    -webkit-box-shadow: 0 0 0 1px rgba(0, 204, 91, 0.2);
+  }
+  70% {
+    -webkit-box-shadow: 0 0 0 5px rgba(0, 204, 91, 0.2);
+  }
+  100% {
+    -webkit-box-shadow: 0 0 0 1px rgba(0, 204, 91, 0.2);
+  }
+}
+@keyframes pulse {
+  0% {
+    -moz-box-shadow: 0 0 0 1px rgba(0, 204, 91, 0.2);
+    box-shadow: 0 0 0 1px rgba(0, 204, 91, 0.1);
+  }
+  70% {
+    -moz-box-shadow: 0 0 0 5px rgba(0, 204, 91, 0.2);
+    box-shadow: 0 0 0 5px rgba(0, 204, 91, 0.1);
+  }
+  100% {
+    -moz-box-shadow: 0 0 0 1px rgba(0, 204, 91, 0.2);
+    box-shadow: 0 0 0 1px rgba(0, 204, 91, 0.2);
+  }
+}
+.ping-text {
+  width: 80%;
+  font-size: 12px;
+}
+.url-block {
+  width: 50%;
+}
+.block-value {
+  display: flex;
+  justify-content: space-between;
+}
+.brokers {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.options {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.fee {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.header-value {
+  margin-bottom: 5px;
+}
+.value {
+  background-color: #F4F5F7;
+  padding: 2px 10px;
+  border-radius: 5px;
+  font-size: 12px;
+  color: #0E5FDA;
+}
+
+.green {
+  color: #00CC5B;
+}
+.yellow {
+  color: #E8C900;
+}
+.red {
+  color: #FD2B2B;
+}
+.grey {
+  color: #C7C7C7;
+}
+.blue {
+  color: #0E5FDA;
+}
+.tx-8 {
+  font-size: 8px;
 }
 </style>
--- client/stories/index.stories.js
+++ client/stories/index.stories.js
@@ -275,111 +275,25 @@ storiesOf('Header', module).add('Purse', () => ({
 storiesOf('Blocks', module).add('BlockOracles', () => ({
   components: { BlockOracles },
   template: `<BlockOracles
-              :data="data"
+              :currencyFirst="currencyFirst"
+              :currencySecond="currencySecond"
+              :ratingValue="ratingValue"
+              :pingValue="pingValue"
+              :urlOracles="urlOracles"
+              :optionsValue="optionsValue"
+              :brokersValue="brokersValue"
+              :feeValue="feeValue"
               ></BlockOracles>`,
   data() {
     return {
-      data: [
-        {
-          currencyFirst: 'BTC',
-          currencySecond: 'USD',
-          oracleValue: 175,
-          brokerValue: 246,
-          isActive: true,
-          barValue: 75,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'ETH',
-          currencySecond: 'USD',
-          oracleValue: 119,
-          brokerValue: 135,
-          isActive: false,
-          barValue: 55,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'LTC',
-          currencySecond: 'USD',
-          oracleValue: 95,
-          brokerValue: 85,
-          isActive: false,
-          barValue: 15,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'EOS',
-          currencySecond: 'USD',
-          oracleValue: 25,
-          brokerValue: 35,
-          isActive: false,
-          barValue: 27,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'BCH',
-          currencySecond: 'USD',
-          oracleValue: 39,
-          brokerValue: 35,
-          isActive: false,
-          barValue: 15,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'XRP',
-          currencySecond: 'USD',
-          oracleValue: 75,
-          brokerValue: 85,
-          isActive: false,
-          barValue: 31,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'TRX',
-          currencySecond: 'USD',
-          oracleValue: 42,
-          brokerValue: 65,
-          isActive: false,
-          barValue: 15,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'DASH',
-          currencySecond: 'USD',
-          oracleValue: 29,
-          brokerValue: 45,
-          isActive: false,
-          barValue: 13,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'NEO',
-          currencySecond: 'USD',
-          oracleValue: 12,
-          brokerValue: 25,
-          isActive: false,
-          barValue: 9,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'ATOM',
-          currencySecond: 'USD',
-          oracleValue: 75,
-          brokerValue: 65,
-          isActive: false,
-          barValue: 16,
-          barVariant: 'success',
-        },
-        {
-          currencyFirst: 'XEM',
-          currencySecond: 'USD',
-          oracleValue: 19,
-          brokerValue: 35,
-          isActive: false,
-          barValue: 10,
-          barVariant: 'success',
-        },
-      ],
+      currencyFirst: 'BTC',
+      currencySecond: 'USD',
+      ratingValue: 2,
+      pingValue: 62,
+      urlOracles: 'https://oracleoftheben',
+      optionsValue: 250,
+      brokersValue: 120,
+      feeValue: 0.25,
     };
   },
 }));
