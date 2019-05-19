ada382aaacf0042389d47c5e14fbf48b69370705
Denis Sel <sfaiross@yandex.ru>
2019-05-19 21:39:07 +0200

    Add component BlockHeader

--- client/src/components/ui-components/Blocks/BlockHeader.vue
+++ client/src/components/ui-components/Blocks/BlockHeader.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="header">
-    <div class="block-logo">
-      <img src="../../../assets/logo.png" alt="">
-    </div>
-    <div class="block-navigation">
-      <Navigation></Navigation>
-    </div>
-    <div class="block-purse">
-      <Purse></Purse>
-    </div>
-    <div class="block-helpers">
-      <div class="block-alert">
-        <Bell :width="16" :height="16"></Bell>
+    <div class="header-left">
+      <div class="block-logo">
+        <img src="../../../assets/logo.png" alt="">
       </div>
-      <div>
-        <div class="block-question">
-          <QuestionCircle :width="16" :height="16"></QuestionCircle>
+      <div class="block-navigation">
+        <Navigation
+          :activePage="activePage"
+        ></Navigation>
+      </div>
+    </div>
+    <div class="header-right">
+      <div class="block-purse">
+        <Purse
+          :balanceValue="balanceValue"
+          :currency="currency"
+          :walletAddress="walletAddress">
+          >
+        </Purse>
+      </div>
+      <div class="block-helpers">
+        <div class="block-alert">
+          <Bell :width="16" :height="16"></Bell>
         </div>
-        <div class="block-lang">
-          <b-form-select v-model="selected" :options="options" disabled-field></b-form-select>
+        <div class="block-control">
+          <div class="block-question">
+            <QuestionCircle :width="16" :height="16"></QuestionCircle>
+          </div>
+          <div class="block-lang">
+            <b-form-select v-model="selected" :options="options" disabled-field></b-form-select>
+          </div>
         </div>
       </div>
     </div>
@@ -47,7 +58,11 @@ export default {
         { value: 'Ru', text: 'Ru' },
         { value: 'Der', text: 'Der' },
         { value: 'Fr', text: 'Fr' },
-      ]
+      ],
+      balanceValue: 230.23,
+      currency: 'GIC',
+      walletAddress: 'DFvstVbsTjdbFGVlstdkeGHbaGdtwBkshr',
+      activePage: 'Dashboard',
     }
   }
 }
@@ -56,7 +71,7 @@ export default {
 <style scoped>
 .header {
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   height: 70px;
@@ -65,13 +80,40 @@ export default {
   background: -o-linear-gradient(right, #0E5FDA, #07306D);
   background: linear-gradient(to left, #0E5FDA, #07306D);
 }
+.header-left {
+  display: flex;
+  align-items: center;
+}
+.header-right {
+  display: flex;
+  align-items: center;
+}
+.block-logo {
+  margin-left: 20px;
+}
+.block-navigation {
+  padding-top: 15px;
+  padding-left: 20px;
+}
+.block-helpers {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 160px;
+}
 .block-alert {
   display: flex;
   align-items: center;
+  padding-left: 10px;
 }
 .block-alert > svg {
   color: #ffffff;
   opacity: 0.7;
+}
+.block-control {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .block-question {
   display: flex;
@@ -81,6 +123,9 @@ export default {
   color: #ffffff;
   opacity: 0.7;
 }
+.block-lang {
+  margin-right: 15px;
+}
 /deep/.custom-select {
   margin-left: 10px;
   font-size: 0.8em;
@@ -88,9 +133,16 @@ export default {
   background: 0;
   background-color: rgba(255, 255, 255, 0.1);
   color: #ffffff;
+  padding-right: 10px;
+  width: 60px;
 }
 /deep/.custom-select > option {
   color: #4F4F4F;
   background-color: #ffffff;
+}
+@media screen and (max-width: 1140px) {
+  .block-navigation {
+    padding-top: 0;
+  }
 }
 </style>
